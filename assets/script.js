347771e77b65091dbb808da45b611ec66dab5959
Colby Sanchez Wagenbach <csanchezwagenbach@gmail.com>
2022-12-07 20:20:41 -0800

    successfully making call to geocode api and returning lat and lon from database when sending it a city name input by the user in search area, logging lat and lon in console, will save to variable and return that data to global scope

--- assets/script.js
+++ assets/script.js
@@ -6,4 +6,23 @@ var searchInput = document.querySelector("#search-term");
 var searchButton = document.querySelector("#search-button");
 var savedCitiesDisplay = document.querySelector("#saved-cities-list");
 
+var searchTerm; //This will be the saved, trimmed, and returned value of the search input
+
 var apiKey = "f024b9c17a84301bd1b8cac7935e5c74";
+
+
+searchButton.addEventListener("click", function () {
+    searchTerm = searchInput.value;
+    console.log(searchInput.value)
+    console.log(searchTerm);
+    var geoCodeUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + searchTerm + "&limit=1&appid=" + apiKey;
+    fetch(geoCodeUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then (function (data) {
+            console.log(data);
+            console.log(data[0].lat);
+            console.log(data[0].lon);
+        });
+});
