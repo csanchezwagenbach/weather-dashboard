be9deb4dafc39297042f3019bcc97a29137b1cbd
Colby Sanchez Wagenbach <csanchezwagenbach@gmail.com>
2022-12-07 20:01:31 -0800

    added queryselectors for all html elements to be interacted with, completed html skeleton for webpage, ready to begin making test api requests

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,9 @@
 console.log("Hello, world");
 
-
+var currentWeatherDisplay = document.querySelector("#current-weather-display");
+var futureForeCastDisplay = document.querySelector("#future-forecast-display");
+var searchInput = document.querySelector("#search-term");
+var searchButton = document.querySelector("#search-button");
+var savedCitiesDisplay = document.querySelector("#saved-cities-list");
 
 var apiKey = "f024b9c17a84301bd1b8cac7935e5c74";
