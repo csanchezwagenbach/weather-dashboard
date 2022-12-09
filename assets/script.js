a6f07ea48301dfca673df26ef36f217cac008ffb
Colby Sanchez Wagenbach <csanchezwagenbach@gmail.com>
2022-12-08 18:00:03 -0800

    spent some time working with scoping, objects are in local storage so shouldn't be a difficulty pulling them to a variable scoped appropriately. going to work on that angle when I return

--- assets/script.js
+++ assets/script.js
@@ -11,7 +11,29 @@ var searchTerm; //This will be the saved, trimmed, and returned value of the sea
 // var searchLon = saveLon();
 var apiKey = "f024b9c17a84301bd1b8cac7935e5c74";
 
-function checkStorage () {
+function getSearchInfo() {
+    searchTerm = searchInput.value;
+    console.log(searchInput.value)
+    console.log(searchTerm);
+    var geoCodeUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + searchTerm + "&limit=1&appid=" + apiKey;
+    var searchCity = fetch(geoCodeUrl)
+                    .then(function (response) {
+                     return response.json();
+                    })
+                    .then(function (data) {
+                        var searchCity = {
+                            name: data[0].name,
+                            latitude: data[0].lat,
+                            longitude: data[0].lon
+                        }
+                        console.log(searchCity);
+                        return searchCity;
+                    })
+            console.log(searchCity);
+        }
+
+
+function checkStorage() {
     var savedCities = localStorage.getItem("saved cities");
     if (savedCities) {
         savedCities = JSON.parse(savedCities);
@@ -19,26 +41,31 @@ function checkStorage () {
         savedCities = [];
     }
     return savedCities;
+};
+
+function getCurrentCityDisplay() {
+    var currentCity = localStorage.getItem("search city");
+    currentCity = JSON.parse(currentCity);
+    return currentCity;
 }
 
-searchButton.addEventListener("click", function () {
-    searchTerm = searchInput.value;
-    console.log(searchInput.value)
-    console.log(searchTerm);
-    var geoCodeUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + searchTerm + "&limit=1&appid=" + apiKey;
-    fetch(geoCodeUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then (function (data) {
-            var newCity = {
-                name: data[0].name,
-                latitude: data[0].lat,
-                longitude: data[0].lon
-            }
-            var savedCities = checkStorage();
-            savedCities.push(newCity);
-            localStorage.setItem("search city", JSON.stringify(newCity))
-            localStorage.setItem("saved cities", JSON.stringify(savedCities)) 
-        }
-    )});
+
+searchButton.addEventListener("click", getSearchInfo);
+console.log(searchCity);
+    
+        //     var savedCities = checkStorage();
+        //     savedCities.push(newCity);
+        //     localStorage.setItem("search city", JSON.stringify(newCity));
+        //     localStorage.setItem("saved cities", JSON.stringify(savedCities));
+        //     var currentCity = getCurrentCityDisplay();
+        //     console.log(currentCity);
+        //     var searchLat = currentCity.latitude;
+        //     var searchLon = currentCity.longitude;
+        //     console.log(searchLat);
+        //     console.log(searchLon);
+        //     var currentWeatherUrl = "api.openweathermap.org/data/2.5/forecast/daily?lat=" + searchLat + "&lon=" + searchLon + "&cnt=6&appid=" + apiKey
+        //     console.log(currentWeatherUrl);
+        // }
+        // )
+
+
