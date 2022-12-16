38ef9cb4c8b986857d5e86cb8f249c853bfd6ccb
Colby Sanchez Wagenbach <csanchezwagenbach@gmail.com>
2022-12-15 16:24:29 -0800

    list items functioning as intended and printing relevant weather for users, app is functional at minimal level

--- assets/script.js
+++ assets/script.js
@@ -85,7 +85,7 @@ function printSavedCities() {
     }
 }
 
- savedCitiesDisplay.addEventListener("click", ".list-group-item", searchClickedCity)
+ savedCitiesDisplay.addEventListener("click", searchClickedCity)
 
 
 function renderPage() {
@@ -94,10 +94,107 @@ function renderPage() {
     printSavedCities();
 }
 
-function searchClickedCity (event) {
-    var clickedCity = $(event.target);
-    var newCity = clickedCity.text.valueOf();
-    console.log(newCity);
+async function searchClickedCity (event) {
+    var clickedCity = event.target.textContent;
+    console.log(clickedCity);
+    searchTerm = clickedCity;
+    var geoCodeUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + searchTerm + "&limit=1&appid=" + geoApiKey;
+    await fetch(geoCodeUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            var searchCity = {
+                name: data[0].name,
+                latitude: data[0].lat.toFixed(2),
+                longitude: data[0].lon.toFixed(2),
+            }
+            console.log(searchCity);
+            localStorage.setItem("search city", JSON.stringify(searchCity));
+        })
+    var currentCity = getCurrentCity();
+    console.log(currentCity);
+    var searchLat = currentCity.latitude;
+    var searchLon = currentCity.longitude;
+    var currentWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + searchLat + "&lon=" + searchLon + "&units=imperial&appid=" + weatherApiKey
+    await fetch(currentWeatherUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
+            var unix = data.dt
+            var currentWeather = {
+                name: data.name,
+                date: dayjs.unix(unix).format("MMM D, YYYY"),
+                temp: data.main.temp,
+                feels_like: data.main.feels_like,
+                humidity: data.main.humidity,
+                wind: data.wind.speed,
+                icon: data.weather[0].icon,
+            }
+            localStorage.setItem("current weather", JSON.stringify(currentWeather));
+        })
+    var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + searchLat + "&lon=" + searchLon + "&units=imperial&appid=" + weatherApiKey
+    console.log(forecastUrl);
+    await fetch(forecastUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            var forecasts = [];
+            var unixOne = data.list[7].dt
+            console.log(dayjs.unix(unixOne).format("MMM D, YYYY"))
+            var forecastOne = {
+                date: dayjs.unix(unixOne).format("MMMM D, YYYY"),
+                temp: data.list[7].main.temp,
+                humidity: data.list[7].main.humidity,
+                wind: data.list[7].wind.speed,
+                icon: data.list[7].weather[0].icon
+            }
+            console.log(forecastOne);
+            forecasts.push(forecastOne);
+            console.log(forecasts);
+            var unixTwo = data.list[15].dt
+            var forecastTwo = {
+                date: dayjs.unix(unixTwo).format("MMMM D, YYYY"),
+                temp: data.list[15].main.temp,
+                humidity: data.list[15].main.humidity,
+                wind: data.list[15].wind.speed,
+                icon: data.list[15].weather[0].icon
+            }
+            forecasts.push(forecastTwo);
+            var unixThree = data.list[23].dt
+            var forecastThree = {
+                date: dayjs.unix(unixThree).format("MMMM D, YYYY"),
+                temp: data.list[23].main.temp,
+                humidity: data.list[23].main.humidity,
+                wind: data.list[23].wind.speed,
+                icon: data.list[23].weather[0].icon
+            }
+            forecasts.push(forecastThree);
+            var unixFour = data.list[31].dt
+            var forecastFour = {
+                date: dayjs.unix(unixFour).format("MMMM D, YYYY"),
+                temp: data.list[31].main.temp,
+                humidity: data.list[31].main.humidity,
+                wind: data.list[31].wind.speed,
+                icon: data.list[31].weather[0].icon
+            }
+            forecasts.push(forecastFour);
+            var unixFive = data.list[39].dt
+            var forecastFive = {
+                date: dayjs.unix(unixFive).format("MMMM D, YYYY"),
+                temp: data.list[39].main.temp,
+                humidity: data.list[39].main.humidity,
+                wind: data.list[39].wind.speed,
+                icon: data.list[39].weather[0].icon
+            }
+            forecasts.push(forecastFive);
+            localStorage.setItem("forecasts", JSON.stringify(forecasts));
+        })
+    renderPage();
 }
 
 searchButton.addEventListener("click", async function () {
@@ -206,5 +303,7 @@ searchButton.addEventListener("click", async function () {
         })
     renderPage();
 })
+
+savedCitiesDisplay.addEventListener("click", searchClickedCity);
 
 renderPage();
